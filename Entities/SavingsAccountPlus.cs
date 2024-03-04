822e9ae56e47803b554355c02752eb95e2894091
DaviihRodrigueSs <davi.devrodrigues@gmail.com>
2024-03-04 17:44:31 -0300

    ClasseSelada

---
+++ Entities/SavingsAccountPlus.cs
@@ -0,0 +1,14 @@
+﻿using Course.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasseSelada.Entities
+{
+    internal class SavingsAccountPlus : SavingsAccount
+    {
+       // public override void Withdraw(double amount)
+    }
+}
